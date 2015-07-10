15e488750c8932c39bf294f2ee840e4577e307b6
Matthew Bender <benderm@gmail.com>
2015-07-10 00:31:27 -0600

    nround=500

--- scripts/xgboost_model.R
+++ scripts/xgboost_model.R
@@ -79,7 +79,7 @@ param <- list("objective" = "reg:linear",
               "colsample_bytree" = .8,
               "scale_pos_weight" = 1)
 offset = 5000
-nround = 1500
+nround = 500
 
 
 y = log(y)
@@ -190,4 +190,4 @@ predTotal = pred + pred2 + pred3 + pred4 + pred5 + pred6 + pred7 + pred8 + pred9
 # Output submission
 predTotal = format(predTotal, digits=8,scientific=F)
 submission = data.frame(Id = test$Id,Hazard=predTotal)
-write.csv(submission,file='../submissions/xgboost_model_9.csv', quote=FALSE,row.names=FALSE)
+write.csv(submission,file='../submissions/xgboost_model_10.csv', quote=FALSE,row.names=FALSE)
