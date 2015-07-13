4a71e1c212778927899d13172ae06e4dd099cfab
Matthew Bender <benderm@gmail.com>
2015-07-12 23:39:15 -0600

    "colsample_bytree" = .85
    score:  0.384223

--- scripts/xgboost_model.R
+++ scripts/xgboost_model.R
@@ -82,10 +82,10 @@ teind = (nrow(train)+1):nrow(x)
 param <- list("objective" = "reg:linear",
               "nthread" = 8,
               "bst:eta" = .01,
-              "bst:max_depth" = 10,
+              "bst:max_depth" = 9,
               "min_child_weight" = 5,
               "subsample" = .8,
-              "colsample_bytree" = .8,
+              "colsample_bytree" = .85,
               "scale_pos_weight" = 1)
 offset = 5000
 nround = 2000
@@ -200,4 +200,4 @@ predTotal = pred + pred2 + pred3 + pred4 + pred5 + pred6 + pred7 + pred8 + pred9
 # Output submission
 predTotal = format(predTotal, digits=8, scientific=F)
 submission = data.frame(Id = test$Id,Hazard=predTotal)
-write.csv(submission,file='../submissions/xgboost_model_25.csv', quote=FALSE,row.names=FALSE)
+write.csv(submission,file='../submissions/xgboost_model_26.csv', quote=FALSE,row.names=FALSE)
